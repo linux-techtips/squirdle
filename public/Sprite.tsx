6e458094f94faa60cd05eb7fd42717e797ecd4fd
Carter Vavra <vavraacademy@gmail.com>
2026-03-14 15:41:37 -0500

    added spritesheet generator

--- public/Sprite.tsx
+++
@@ -1,5 +0,0 @@
-export function Sprite({ pokemon }: { pokemon: string }) {
-  return (
-    <img src={`/sprites/${pokemon}.png`} width="96px" height="96px"></img>
-  );
-}
---
+++ scripts/spritesheet.ts
@@ -0,0 +1,17 @@
+import * as path from "path";
+import { $ } from "bun";
+
+export async function generateSpriteSheet(dir: string, name: string) {
+  const files = Array.from({ length: 649 }, (_, i) => path.join(dir, `${i + 1}.png`));
+
+  await $`montage ${files} -geometry 96x96+0+0 -background none -tile 26x25 ${name}`;
+}
+
+async function main() {
+  const spritesDir = process.argv[2] ?? path.join(import.meta.dir, "sprites");
+  const sheetName = process.argv[3] ?? path.join(import.meta.dir, "../public/static/spritesheet.png");
+
+  await generateSpriteSheet(spritesDir, sheetName);
+}
+
+if (import.meta.main) main();
