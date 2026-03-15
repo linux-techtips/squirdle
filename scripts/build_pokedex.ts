9a9c5a9ec13965669c8ce68f1dc76163a82db18f
Carter Vavra <vavraacademy@gmail.com>
2026-03-15 14:13:43 -0500

    fixed bug in fetch_pokedex and made pokedex.json an asset

--- scripts/build_pokedex.ts
+++ scripts/build_pokedex.ts
@@ -1,11 +1,11 @@
 import { Database } from "bun:sqlite";
-import * as path from "path";
 
-async function main() {
-  const db = new Database("pokedex.db", { strict: true });
+import * as util from "util";
 
+async function build_pokedex(db_path: string, pokedex_path: string) {
+  const db = new Database(db_path, { strict: true });
   db.run(`
-    CREATE TABLE pokemon (
+    CREATE TABLE IF NOT EXISTS pokemon (
       id INTEGER PRIMARY KEY,
       name TEXT NOT NULL,
       type1 TEXT NOT NULL,
@@ -21,15 +21,32 @@ async function main() {
     VALUES ($id, $name, $type1, $type2, $height, $weight, $generation)
   `);
 
-  const bulkInsert = db.transaction((pokemon) => {
+  const bulkInsert = db.transaction((pokemon: any[]) => {
     for (const [i, poke] of pokemon.entries()) {
       if (!poke) continue;
       insert.run({ id: i + 1, ...poke });
     }
   });
 
-  const { default: pokedex } = await import("./pokedex.json");
-  bulkInsert(pokedex);
+  const file = Bun.file(pokedex_path);
+  const pokemon = await file.json();
+  bulkInsert(pokemon);
+}
+
+async function main() {
+  const { values, positionals } = util.parseArgs({
+    args: Bun.argv.slice(2),
+    options: {
+      db: { type: "string" },
+    },
+    strict: true,
+    allowPositionals: true,
+  });
+
+  const db_path = values.db ?? process.env.DATABASE_URL ?? "pokedex.db";
+  const [pokedex_path = "pokedex.json"] = positionals;
+
+  await build_pokedex(db_path, pokedex_path);
 }
 
 if (import.meta.main) main();
