887bfef2d780d57a3d69af34abfb292dbca5a444
Jannette Carranza <jannettecarranza@gmail.com>
2026-05-07 20:46:56 -0500

    profile search is currently working but looks wonky, need to make it look pretty

--- src/client/pages/Profilescreen.tsx
+++ src/client/pages/Profilescreen.tsx
@@ -1,15 +1,27 @@
-import { Navbar } from "@/client/components";
-import { Router } from "@/client";
-import { Auth } from "@/client";
-import * as React from "react";
+import type { Profile as ProfileType } from "@/types";
+
+import { Navbar, Sprite } from "@/client/components";
+import { Router, Auth } from "@/client";
 import { useMusic } from "@/client/pages/MusicContext";
+
+import * as hooks from "@/client/hooks";
+import * as React from "react";
 
 export default function Profile() {
   const router = Router.use();
   const auth = Auth.use();
   const { setMusicOn } = useMusic();
 
-   React.useEffect(() => {
+  const [query, setQuery] = React.useState("");
+  const debouncedQuery = hooks.useDebounce(250, query);
+
+  const { data } = hooks.useFetch(
+    `/api/profiles/search?query=${debouncedQuery}`
+  );
+
+  const profiles = (data ?? []) as ProfileType[];
+
+  React.useEffect(() => {
     if (!auth.state) {
       router.navigate("/signin");
     }
@@ -30,25 +42,15 @@ export default function Profile() {
           router.navigate("/signin");
         }}
       />
-      
 
       <main className="page">
         <section className="card">
           <h1 className="title">My Profile</h1>
+
+          <Sprite poke_id={auth.state.favorite_pokemon_id} />
+
           <p>
-            <strong>Username:</strong> AshKetchum
-          </p>
-          <p>
-            <strong>Games Played:</strong> 24
-          </p>
-          <p>
-            <strong>Win Rate:</strong> 83%
-          </p>
-          <p>
-            <strong>Current Streak:</strong> 6
-          </p>
-          <p>
-            <strong>Best Streak:</strong> 12
+            <strong>Username:</strong> {auth.state.username}
           </p>
         </section>
 
@@ -56,9 +58,25 @@ export default function Profile() {
           <h2>Search Other Players</h2>
 
           <div className="guess-bar">
-            <input type="text" placeholder="Search username..." />
-            <button className="primary-btn">Search</button>
+            <input
+              type="text"
+              placeholder="Search username..."
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+            />
           </div>
+
+          {query && profiles.length === 0 && (
+            <p>No players found.</p>
+          )}
+
+          {profiles.map((profile) => (
+            <div className="player-result-card" key={profile.id}>
+              <Sprite poke_id={profile.favorite_pokemon_id} />
+
+              <h3>{profile.username}</h3>
+            </div>
+          ))}
         </section>
       </main>
     </>
